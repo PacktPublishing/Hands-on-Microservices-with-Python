d9e612365a4b64786ad7bcef233589b4756c08b8
peter fisher <hello@websomatic.co.uk>
2018-06-17 12:53:32 +0100

    Updating structure

--- app/app.py
+++ app/app.py
@@ -13,7 +13,6 @@ app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key",
     SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:test@db/order',
-    SQLALCHEMY_TRACK_MODIFICATIONS='False'
 ))
 db.init_app(app)
 
--- app/models.py
+++ app/models.py
@@ -33,7 +33,6 @@ class Customer(db.Model):
             'email_address': self.email
         }
 
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
