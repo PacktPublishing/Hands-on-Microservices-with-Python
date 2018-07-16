082f37182cef7ecbcca795fbb89e92d92702a1ee
peterfisher <c3T-y7J-vo3-qpg>
2018-07-16 08:27:12 +0100

    Adding order add item API

--- app/order_api/routes.py
+++ app/order_api/routes.py
@@ -1,7 +1,8 @@
 from flask import jsonify, request
 from . import order_api_blueprint
-from models import db, Order
-
+from models import db, Order, OrderItem
+from app import load_user_from_request
+from flask_login import login_required, current_user
 
 @order_api_blueprint.route('/api/orders', methods=['GET'])
 def orders():
@@ -14,14 +15,98 @@ def orders():
 
     return response
 
+@login_required
+@order_api_blueprint.route('/api/order/add-item', methods=['POST'])
+def order_add_item():
 
-@order_api_blueprint.route('/api/order/<id>', methods=['GET'])
-def order(id):
-    item = Order.query.filter_by(id=id).first()
-    if item is not None:
-        response = jsonify(item.to_json())
+    if not current_user.is_authenticated:
+        return jsonify({'message': 'Access Denied'}), 401
+
+    p_id = int(request.form['product_id'])
+    qty = int(request.form['qty'])
+    u_id = int(current_user.id)
+
+    # Find open order
+    known_order = Order.query.filter_by(user_id=u_id, is_open=1).first()
+
+    if known_order is None:
+        # Create the order
+        known_order = Order()
+        known_order.is_open = True
+        known_order.user_id = u_id
+
+        order_item = OrderItem()
+        order_item.product_id = p_id
+        order_item.quantity = qty
+        known_order.items.append(order_item)
+
     else:
-        response = jsonify({'message': 'Cannot find order'}), 404
+        found = False
+        # Check if we already have an order item with that product
+        for item in known_order.items:
+
+            if item.product.id == p_id:
+                found = True
+                item.quantity += qty
+
+        if found is False:
+            order_item = OrderItem()
+            order_item.product_id = p_id
+            known_order.items.append(order_item)
+
+    db.session.add(known_order)
+    db.session.commit()
+
+    response = jsonify({'result': known_order.to_json()})
+
+    return response
+
+
+@login_required
+@order_api_blueprint.route('/api/order/update', methods=['POST'])
+def order_update():
+
+    if not current_user.is_authenticated:
+        return jsonify({'message': 'Access Denied'}), 401
+
+    items = request.form['items']
+
+    u_id = int(current_user.id)
+
+    # Find open order
+    known_order = Order.query.filter_by(user_id=u_id, is_open=1).first()
+
+    if known_order is None:
+        # Create the order
+        known_order = Order()
+        known_order.create()
+
+    # Delete any exiting order items
+    known_order.items = []
+
+    for item in items:
+        order_item = OrderItem(item.product.id, item.quantity)
+        known_order.items.append(order_item)
+
+    db.session.add(known_order)
+    db.session.commit()
+
+    response = jsonify({'result': known_order.to_json()})
+
+    return response
+
+@login_required
+@order_api_blueprint.route('/api/order', methods=['GET'])
+def order():
+    if not current_user.is_authenticated:
+        return jsonify({'message': 'Access Denied'}), 401
+
+    open_order = Order.query.filter_by(user_id=current_user.id, is_open=1).first()
+
+    if open_order is None:
+        response = jsonify({'message': 'No order found'})
+    else:
+        response = jsonify({'result': open_order.to_json()})
 
     return response
 
