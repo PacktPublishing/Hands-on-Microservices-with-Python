ee8d080b656e061c5af4afdf314b17bae3cbfd61
Boldizs Peter <Boldizs.Peter@marktsoft.hu>
2022-03-31 12:34:02 +0200

    Downgraded to Flask-Login==0.4.1

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask
+from flask import Flask
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from frontend import frontend_blueprint
@@ -11,14 +11,12 @@ login_manager.login_view = 'main.login'
 
 bootstrap = Bootstrap(app)
 
-
 app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key",
-    PRODUCT_SERVICE= 'http://192.168.99.100:8081'
+    PRODUCT_SERVICE='http://0.0.0.0:8081'
 ))
 
 app.register_blueprint(frontend_blueprint)
 
 app.run(debug=True, host='0.0.0.0')
-
